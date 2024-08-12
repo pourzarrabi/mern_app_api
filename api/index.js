299b63af38fd2619052a2eee248d620b8f6efaf1
pourzarrabi <pourzarrabi@gmail.com>
2024-08-13 01:52:32 +0330

    Completed

---
+++ api/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { connectDB } from "../config/db.js";
+import productsRoutes from "../routes/product.route.js";
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+app.use("/api/products", productsRoutes);
+
+export default app;
